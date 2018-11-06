40f1f660f8366851963e7441c8e055db8ddcc77b
rykn0wxx <raurici666@gmail.com>
2018-11-06 09:50:37 +0800

    init

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        router-link
+        to="/about">
+        <span class="mr-2">Latest Release</span>
+        <v-icon>open_in_new</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
